67ff00729b48a1754b1733dedde64b741e5e3bdc
Webdeveloperintraining <sil22006@byui.edu>
2024-01-10 19:22:38 -0800

    w1 Learning Activities done

--- server.js
+++ server.js
@@ -9,11 +9,24 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const expressLayouts = require("express-ejs-layouts")
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.use(static)
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views roots
+
+// Index route
+app.get("/", function(req,res){
+  res.render("index", {title:"Home"})
+})
 
 /* ***********************
  * Local Server Information
