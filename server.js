a4bc959179a8edb1976740da3702d5049e8a5c16
Webdeveloperintraining <sil22006@byui.edu>
2024-01-25 21:16:35 -0800

    error handling activities comlete

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const static = require("./routes/static")
 const inventoryRoute = require('./routes/inventoryRoute')
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
-//const utilities = require(".utilities");
+const utilities = require("./utilities/");
 /* ***********************
  * Routes
  *************************/
@@ -31,7 +31,13 @@ app.set("layout", "./layouts/layout") // not at views roots
 app.use("/inv", inventoryRoute)
 
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 
 /* ***********************
  * Local Server Information
@@ -45,4 +51,19 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
 })
