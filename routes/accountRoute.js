bd8ec67f6e068550b4070f5d0495ea7f34b3f08c
Webdeveloperintraining <sil22006@byui.edu>
2024-02-01 10:48:46 -0800

    learning activities 2 complete

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,13 +1,17 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
+const regValidate = require('../utilities/account-validation')
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/");
 
 // Route to build account
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/registration", utilities.handleErrors(accountController.buildRegister));
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register',
+regValidate.registationRules(),
+regValidate.checkRegData,
+utilities.handleErrors(accountController.registerAccount))
 
 
 module.exports = router;
