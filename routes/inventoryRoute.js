37dc9fbceedd68cfe46767d495ab7f10f3da848e
Webdeveloperintraining <sil22006@byui.edu>
2024-02-03 10:02:09 -0800

    Routes updated

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,6 +1,7 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
+const classValidate = require('../utilities/inventory-validation')
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/");
 
@@ -12,12 +13,15 @@ router.get("/detail/:inv_id",utilities.handleErrors(invController.buildByDetails
 /* Management Area */
 router.get("/inv",utilities.handleErrors(invController.buildManagement));
 
-router.get("/add-inventory",utilities.handleErrors(invController.newInventory));
+router.get("/add-inventory",utilities.handleErrors(invController.buildInventory));
 //router.post("/inv/add-inventory",utilities.handleErrors(invController.newInventory));
 
-router.get("/add-classification",utilities.handleErrors(invController.newClassification));
+router.get("/add-classification",utilities.handleErrors(invController.buildClassification));
 //router.post("/inv/add-classification",utilities.handleErrors(invController.newClassification));
-
+router.post('/add-classification',
+classValidate.ClassificationRules(),
+classValidate.checkClassificationData,
+utilities.handleErrors(invController.addNewClassification))
 
 //router.get("/detail",utilities.handleErrors(invController.buildByDetails));
 module.exports = router;
