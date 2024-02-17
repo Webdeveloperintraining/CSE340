a001671367f0b3e6f5ad8272cddf043c46129715
Webdeveloperintraining <sil22006@byui.edu>
2024-02-17 13:02:14 -0800

    standarizing form view

--- public/js/account.js
+++ public/js/account.js
@@ -26,7 +26,7 @@
  function buildAccountTypeForm(data) { 
     let accountDisplay = document.getElementById("accountTypeForm"); 
     let accountData = '<h2>Change Account Type</h2>';
-    accountData += '<form action="/account/accounts-management" class="management-forms" method="post">';
+    accountData += '<form action="/account/accounts-management" class="general-form" method="post">';
     accountData += `<label>Account Firstname:</label><input readonly value="${data.account_firstname}"> </input>`;
     accountData += `<label>Account Lastname:</label><input readonly value="${data.account_lastname}"></input>`;
     accountData += '<label>Account Type:</label>';
@@ -52,7 +52,7 @@ function buildDeleteAccountForm(data) {
     let accountDisplay = document.getElementById("accountDeleteForm"); 
     let accountData = '<br><h2>Delete Account</h2>';
     accountData += '<h3><strong>NOTE: DELETING AN ACCOUNT IS IRREVERSIBLE</strong></h3>',
-    accountData += '<form action="/account/account-delete" class="management-forms" method="post" onSubmit="if(!confirm("Are you sure you want to delete this account?")){return false;}">'
+    accountData += '<form action="/account/account-delete" class="general-form" method="post" onSubmit="if(!confirm("Are you sure you want to delete this account?")){return false;}">'
     accountData += `<label>Account Firstname:</label><input readonly value="${data.account_firstname}"> </input>`,
     accountData += `<label>Account Lastname: </label><input readonly value="${data.account_lastname}"></input>`,
     accountData += `<label>Account Email:</label><input readonly value="${data.account_email}"></input>`,
